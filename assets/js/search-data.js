73c9378de29bc7eff2b3661238fad8fca17b1839
matteobiagetti <47861629+matteobiagetti@users.noreply.github.com>
2026-01-09 09:20:47 +0000

    Deploying to gh-pages from @ matteobiagetti/matteobiagetti.github.io@8400e5294df5535ca4fdd4258443ecdbc34e9bef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,7 +92,7 @@ ninja.data = [{
         title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/matteo.biagetti", "_blank");
+          window.open("https://github.com/matteobiagetti", "_blank");
         },
       },{
         id: 'social-linkedin',
